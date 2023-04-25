6d841c81286598cb075a187b90fa9784de1523b2
mikhaelHan <hancharuk270185@mail.ru>
2023-04-25 17:02:50 +0300

    feat: added eslint to the project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './assets/styles/style.scss';
 const one = 123;
 
 console.log(one);
+
 const body = document.querySelector('body');
 body.style.background = 'green';
 body.textContent += 'virtual-keyboard !';
