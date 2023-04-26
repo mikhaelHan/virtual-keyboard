19a578ec60b95309b09f3dfaf26066be3d5d06bb
mikhaelHan <hancharuk270185@mail.ru>
2023-04-26 16:15:30 +0300

    feat: created a static keyboard without functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './index.html';
 import './assets/styles/style.scss';
-
-const one = 123;
-
-console.log(one);
-
-const body = document.querySelector('body');
-body.style.background = 'green';
-body.textContent += 'virtual-keyboard !';
